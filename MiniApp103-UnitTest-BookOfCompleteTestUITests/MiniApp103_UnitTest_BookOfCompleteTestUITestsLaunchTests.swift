136d7ed5ff6664b14f4de4151fe970a05b3de8c2
Kota <maedakoutaa@gmail.com>
2022-09-30 16:05:59 +0900

    Initial Commit

---
+++ MiniApp103-UnitTest-BookOfCompleteTestUITests/MiniApp103_UnitTest_BookOfCompleteTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MiniApp103_UnitTest_BookOfCompleteTestUITestsLaunchTests.swift
+//  MiniApp103-UnitTest-BookOfCompleteTestUITests
+//
+//  Created by 前田航汰 on 2022/09/30.
+//
+
+import XCTest
+
+final class MiniApp103_UnitTest_BookOfCompleteTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
